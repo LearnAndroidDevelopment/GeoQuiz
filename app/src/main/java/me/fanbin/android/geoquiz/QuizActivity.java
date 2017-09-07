52ed2a8e75cb6bb0c812690c59e1177dcc98b5da
Fan Bin <fanbin0602@163.com>
2017-09-07 15:06:28 +0800

    practice

--- app/src/main/java/me/fanbin/android/geoquiz/QuizActivity.java
+++ app/src/main/java/me/fanbin/android/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package me.fanbin.android.geoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,22 +21,24 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(
+                Toast toast = Toast.makeText(
                         QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT)
-                        .show();
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(
+                Toast toast = Toast.makeText(
                         QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT)
-                        .show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
     }
